5d7966a0092891de6032bfd1277ab30fdeb1878a
vinodederratech <57748236+vknowd1@users.noreply.github.com>
2022-03-13 22:02:00 +0530

    Create 1.py
    1 question solution

---
+++ 1.py
@@ -0,0 +1,15 @@
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+
+count_input = input('Enter a line: ')
+print( word_count(count_input))
